2b98f58e89e3c7b7b8b75a7d3a2739242c0e8b18
antoine <antoine.boisard8@gmail.com>
2025-03-03 16:32:32 +0100

    add header with welcome message

---
+++ screens/components/ButtonHalf.js
@@ -0,0 +1,65 @@
+import React from "react";
+import { TouchableOpacity, Text, StyleSheet } from "react-native";
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+import {useEffect} from 'react';
+
+function ButtonHalf({ text, onPress, style }) {
+  const [loaded, error] = useFonts({
+      'LilitaOne-Regular': require('../../assets/fonts/LilitaOne-Regular.ttf'),
+  });
+
+  useEffect(() => {
+    if (loaded || error) {
+        SplashScreen.hideAsync();
+        }
+    }, [loaded, error]);
+
+if (!loaded && !error) {
+    return null;
+} 
+
+return (
+    <TouchableOpacity
+      style={[styles.buttonHalf, style]} 
+    //   onPress={onPress}
+      activeOpacity={0.7}
+    >
+      <Text style={styles.text}>{text}</Text>
+    </TouchableOpacity>
+  );
+}
+export default ButtonHalf;
+
+const styles = StyleSheet.create({
+  buttonHalf: {
+    borderWidth: 2,
+    borderColor: "#000000",
+    width: '80%',
+    height: 50,
+    borderRadius: 10,
+    alignItems: "center",
+    justifyContent: "center",
+    marginVertical: 10,
+    backgroundColor: '#F095B4',
+
+    // Ombre sur iOS
+    shadowColor: "#000",
+    shadowOffset: { width: 4, height: 4 }, 
+    shadowOpacity: 0.4,
+    shadowRadius: 4,
+
+    // Ombre sur Android
+    elevation: 5, 
+
+  },
+  text: {
+    color: "#ffff", 
+    fontFamily: 'LilitaOne-Regular',
+    fontSize : 20,
+    textShadowColor: 'black', 
+    textShadowOffset: { width: 1, height:3 }, 
+    textShadowRadius: 1, 
+    }
+});
+
--- screens/components/HeaderNavigation.js
+++ screens/components/HeaderNavigation.js
@@ -1,8 +1,10 @@
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import { useFonts } from 'expo-font';
 import * as SplashScreen from 'expo-splash-screen';
 import {useEffect} from 'react';
-import { LinearGradient } from 'expo-linear-gradient';
+
+import ButtonHalf from './ButtonHalf'
+import SearchBar from './SearchBar'
 
 const HeaderNavigation = () => {
 
@@ -22,21 +24,17 @@ const HeaderNavigation = () => {
 
  return (
 
-    <LinearGradient
-        colors={['rgba(34,193,195,1)', 'rgba(253,187,45,1)']} // Couleurs du dégradé
-        start={{ x: 0, y: 1 }} // Point de départ du dégradé (0,1 = bas)
-        end={{ x: 0, y: 0 }} // Point d'arrivée du dégradé (0,0 = haut)
-        style={styles.header}
-    >
+    <View style={styles.header}>
         <View style={styles.headerTop}>  
-                <Text style={styles.headerTopKiddiz}>Kiddiz</Text>
-                <View>
-                    <Button title="Go to Connection" onPress={() => navigation.navigate('Connection')}/>
-                </View>
+            <Text style={styles.headerTopKiddiz}>Kiddiz</Text>
+            <View style={styles.headerButton}>
+                <ButtonHalf style={styles.buttonApple} text="Connexion" onPress={() => navigation.navigate('Connection')}/>
             </View>
-            <View style={styles.headerBottom}>
         </View>
-    </LinearGradient>
+        <View style={styles.headerBottom}>
+            <SearchBar/>
+        </View>
+    </View>
 
  );
 }
@@ -44,13 +42,13 @@ const HeaderNavigation = () => {
 const styles = StyleSheet.create({
     header: {
         paddingTop: 50,
-        padding: 20,
     },
     headerTop: {
         display: 'flex',
         flexDirection: 'row',
         justifyContent: 'space-between',
-        alignItems: 'center'
+        alignItems: 'center',
+        marginBottom: 15,
     },
     headerTopKiddiz: {
         fontSize: 60,
@@ -61,6 +59,14 @@ const styles = StyleSheet.create({
         textShadowRadius: 3, textShadowColor: 'black', 
         textShadowOffset: { width: 4, height: 6 }, 
         textShadowRadius: 3, 
+    },
+    headerButton: {
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'end',
+        justifyContent: 'flex-end',
+        width: '50%',
+        height: 60,
     }
 })
 
