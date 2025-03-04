da98afc84dbdfdac858da20cd5ef316b6a32b1d5
Your name <Your email>
2025-03-04 06:47:08 +0100

    persist reducer user

---
+++ reducers/users.js
@@ -0,0 +1,35 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+// Initial state
+const initialState = {
+  value: {
+    firstname: null,
+    lastname: null,
+    email: null,
+    dateOfBirth: null,
+  },
+};
+
+export const userSlice = createSlice({
+  name: 'user',
+  initialState,
+  reducers: {
+    // Action to update user
+    updateUser: (state, action) => {
+      state.value = { ...state.value, ...action.payload };
+    },
+    // Action to reset user
+    resetUser: (state) => {
+      state.value = {
+        firstname: null,
+        lastname: null,
+        email: null,
+        dateOfBirth: null,
+      };
+    },
+  },
+});
+
+// Exporter les actions
+export const { updateUser, resetUser } = userSlice.actions;
+export default userSlice.reducer;
