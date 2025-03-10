61a2143f2edb7f9682249b810c3084a9fe25bd08
Your name <Your email>
2025-03-10 18:15:59 +0100

    save modale filtre

--- screens/components/SearchBar.js
+++ screens/components/SearchBar.js
@@ -1,65 +1,166 @@
-import React from "react";
-import { TouchableOpacity, Text, StyleSheet, TextInput, View } from "react-native";
+import React, { useState, useEffect } from "react";
+import { TouchableOpacity, Text, StyleSheet, TextInput, View, Modal, TouchableWithoutFeedback, Keyboard, Animated } from "react-native";
 import { useFonts } from 'expo-font';
 import * as SplashScreen from 'expo-splash-screen';
-import {useEffect, useState} from 'react';
 import FontAwesome from "react-native-vector-icons/FontAwesome";
 import { useRoute } from '@react-navigation/native';
-// import { useNavigation } from '@react-navigation/native';
 
 function SearchBar({ onSearch }) {
-  const route = useRoute(); // Récupère la route actuelle
+  const route = useRoute(); 
 
   const [loaded, error] = useFonts({
-      'LilitaOne-Regular': require('../../assets/fonts/LilitaOne-Regular.ttf'),
-      'RopaSans-Regular': require('../../assets/fonts/RopaSans-Regular.ttf'),
+    'LilitaOne-Regular': require('../../assets/fonts/LilitaOne-Regular.ttf'),
+    'RopaSans-Regular': require('../../assets/fonts/RopaSans-Regular.ttf'),
   });
 
   useEffect(() => {
     if (loaded || error) {
-        SplashScreen.hideAsync();
-        }
-    }, [loaded, error]);
+      SplashScreen.hideAsync();
+    }
+  }, [loaded, error]);
 
-if (!loaded && !error) {
+  if (!loaded && !error) {
     return null;
-} 
+  }
 
-const [isFocused, setIsFocused] = useState(false);
-const [searchTerm, setSearchTerm] = useState('');
+  const [isFocused, setIsFocused] = useState(false);
+  const [searchTerm, setSearchTerm] = useState('');
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [filters, setFilters] = useState({
+    age: null,
+    category: null,
+  });
+
+  // Animation d'échelle et de couleur
+  const [scale] = useState(new Animated.Value(1));
+  const [backgroundColor, setBackgroundColor] = useState('#fff');  // Couleur de fond par défaut
 
   const handleSearch = () => {
-
-    if (searchTerm.trim()) { // Vérifie que le terme de recherche n'est pas vide
-      onSearch(searchTerm); // Appelle la fonction passée via les props
-      
+    if (searchTerm.trim()) { 
+      onSearch(searchTerm); 
     }
   };
 
-return route.name === 'Home' ? (
+  const openFilterModal = () => {
+    setIsModalVisible(true); 
+  };
+
+  const closeFilterModal = () => {
+    setIsModalVisible(false); 
+  };
+
+  const handleFilterSelection = (filterCategory, value) => {
+    setFilters((prevFilters) => ({
+      ...prevFilters,
+      [filterCategory]: value,
+    }));
+    // Ajouter une animation lors de la sélection
+    Animated.sequence([
+      Animated.timing(scale, {
+        toValue: 0.95, // Réduit légèrement la taille
+        duration: 150,
+        useNativeDriver: true,
+      }),
+      Animated.timing(scale, {
+        toValue: 1, // Retourne à la taille normale
+        duration: 150,
+        useNativeDriver: true,
+      }),
+    ]).start();
+
+    setBackgroundColor('#F0D74D');  // Change la couleur de fond au clic
+  };
+
+  const resetFilters = () => {
+    setFilters({
+      age: null,
+      category: null,
+    });
+    setBackgroundColor('#fff');  // Restaure la couleur de fond par défaut
+  };
+
+  return route.name === 'Home' ? (
     <View style={styles.searchContainer}>
-        <TouchableOpacity
-            style={styles.buttonFilter} 
-            activeOpacity={0.7}
-        >
-            <Text style={styles.buttonFilterText}>Filtres :</Text>
-        </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.buttonFilter} 
+        activeOpacity={0.7}
+        onPress={openFilterModal} 
+      >
+        <Text style={styles.buttonFilterText}>Filtres :</Text>
+      </TouchableOpacity>
+      
       <View style={styles.inputContainer}>
         <TextInput 
-        style={styles.searchBar} 
-        placeholderTextColor="#888" 
-        placeholder="Rechercher..."
-        onFocus={() => setIsFocused(true)}
-        onBlur={() => setIsFocused(false)}
-        onChangeText={setSearchTerm} // Met à jour le terme de recherche
-        value={searchTerm} // Contrôle la valeur du champ
-        >
-        </TextInput>
+          style={styles.searchBar} 
+          placeholderTextColor="#888" 
+          placeholder="Rechercher..."
+          onFocus={() => setIsFocused(true)}
+          onBlur={() => setIsFocused(false)}
+          onChangeText={setSearchTerm} 
+          value={searchTerm} 
+        />
         {isFocused && <FontAwesome name="search" size={20} color="black" style={styles.icon} onPress={handleSearch}/>}
-        </View>
+      </View>
+
+      {/* Modal pour les filtres */}
+      <Modal
+        visible={isModalVisible}
+        animationType="none"
+        transparent={true}
+        onRequestClose={closeFilterModal} 
+      >
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+          <View style={styles.modalOverlay}>
+            <View style={styles.modalContent}>
+              <Text style={styles.modalTitle}>Sélectionner les filtres</Text>
+
+              {/* Bouton pour réinitialiser les filtres */}
+              <TouchableOpacity style={styles.filterOption} onPress={resetFilters}>
+                <Text style={styles.filterText}>Réinitialiser tous les filtres</Text>
+              </TouchableOpacity>
+
+              {/* Filtres d'âge */}
+              {['0 - 3 ans', '4 - 6 ans', '6 - 10 ans', '10 - 14 ans'].map((ageRange) => (
+                <Animated.View 
+                  key={ageRange} 
+                  style={[styles.filterOption, { transform: [{ scale }] }]}
+                >
+                  <TouchableOpacity 
+                    style={[styles.filterOption, { backgroundColor: filters.age === ageRange ? backgroundColor : '#fff' }]} 
+                    onPress={() => handleFilterSelection('age', ageRange)}
+                  >
+                    <Text style={styles.filterText}>{ageRange}</Text>
+                  </TouchableOpacity>
+                </Animated.View>
+              ))}
+
+              {/* Filtres de catégorie */}
+              {['Puériculture', 'Jouets', 'Loisirs'].map((category) => (
+                <Animated.View 
+                  key={category} 
+                  style={[styles.filterOption, { transform: [{ scale }] }]}
+                >
+                  <TouchableOpacity 
+                    style={[styles.filterOption, { backgroundColor: filters.category === category ? backgroundColor : '#fff' }]} 
+                    onPress={() => handleFilterSelection('category', category)}
+                  >
+                    <Text style={styles.filterText}>{category}</Text>
+                  </TouchableOpacity>
+                </Animated.View>
+              ))}
+
+              {/* Bouton pour fermer le modal */}
+              <TouchableOpacity style={styles.closeButton} onPress={closeFilterModal}>
+                <Text style={styles.closeButtonText}>Fermer</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </TouchableWithoutFeedback>
+      </Modal>
     </View>
   ) : null;
 }
+
 export default SearchBar;
 
 const styles = StyleSheet.create({
@@ -82,13 +183,10 @@ const styles = StyleSheet.create({
     borderTopLeftRadius: 10,
     borderBottomLeftRadius: 10,
 
-    // Ombre sur iOS
     shadowColor: "#000",
     shadowOffset: { width: 4, height: 4 },
     shadowOpacity: 0.4,
     shadowRadius: 4,
-
-    // Ombre sur Android
     elevation: 5,
   },
   buttonFilterText: {
@@ -96,7 +194,7 @@ const styles = StyleSheet.create({
     fontSize: 16,
   },
   inputContainer: {
-    width: '70%', // Largeur fixe pour correspondre à la searchBar
+    width: '70%',
     height: 45,
     borderWidth: 1,
     borderColor: "#000000",
@@ -109,17 +207,14 @@ const styles = StyleSheet.create({
     paddingRight: 10,
     position: 'relative',
 
-    // Ombre sur iOS
     shadowColor: "#000",
     shadowOffset: { width: 4, height: 4 },
     shadowOpacity: 0.4,
     shadowRadius: 4,
-
-    // Ombre sur Android
     elevation: 5,
   },
   searchBar: {
-    flex: 1, // Prend toute la largeur disponible
+    flex: 1,
     height: '100%',
     fontFamily: 'Ropa-Sans',
     textAlign: 'left',
@@ -127,7 +222,46 @@ const styles = StyleSheet.create({
     fontSize: 14,
   },
   icon: {
-    position: 'absolute', // Positionne l'icône absolument dans le conteneur
-    right: 10, // Place l'icône à droite
+    position: 'absolute',
+    right: 10,
+  },
+  modalOverlay: {
+    flex: 1,
+    top: '19%',
+    alignItems: 'center',
+    // backgroundColor: 'rgba(0, 0, 0, 0.5)', 
+  },
+  modalContent: {
+    width: '90%',
+    backgroundColor: 'white',
+    borderRadius: 10,
+    padding: 20,
+    borderWidth: 2,
+    borderColor: "#000000",
+  },
+  modalTitle: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+    fontFamily: 'LilitaOne-Regular',
+  },
+  filterOption: {
+    paddingVertical: 8,
+    
+  },
+  filterText: {
+    fontSize: 20,
+    fontFamily: 'RopaSans-Regular',
+  },
+  closeButton: {
+    marginTop: 20,
+    backgroundColor: '#4478A9',
+    paddingVertical: 10,
+    borderRadius: 5,
+  },
+  closeButtonText: {
+    fontSize: 16,
+    textAlign: 'center',
+    color: 'white',
   },
 });
